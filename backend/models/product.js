7bb2fe5e1d19f705bace1f773b472282abdcb8dd
sailesh307 <sailesh953@gmail.com>
2023-09-04 13:22:54 +0530

    product sceme is now more readable

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
+        type: String,
         required: true,
     },
     name: {
@@ -21,15 +20,28 @@ const productSchema = new mongoose.Schema({
     },
     imageUrls: [String], // Array of image URLs
     seller: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        type: String,
         required: true,
     },
     date: {
         type: Date,
         default: Date.now,
-    },    
-  // Additional fields as needed.
+    },
+    // Additional fields as needed.
+});
+
+productSchema.virtual('categoryRef', {
+    ref: 'Category', // reference to 'Category' model
+    localField: 'category', // // Local field to match against 'Category' model's field
+    foreignField: 'name', // Field in the 'Category' model to match against
+    justOne: true, // Only return one category
+});
+
+productSchema.virtual('sellerRef', {
+    ref: 'User', // reference to 'User' model
+    localField: 'seller', // // Local field to match against 'User' model's field
+    foreignField: 'username', // Field in the 'User' model to match against
+    justOne: true, // Only return one user
 });
 
 module.exports = mongoose.model('Product', productSchema);
