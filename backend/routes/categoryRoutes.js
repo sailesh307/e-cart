0fb5775604716f64bd7fe6c533cb774f76af2414
sailesh307 <sailesh953@gmail.com>
2023-09-05 16:41:56 +0530

    auth addded

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/CategoryController');
+const authMiddleware = require('../middleware/authMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
 
-// Create a new category
-router.post('/', categoryController.createCategory);
-
+///////////////////// Common /////////////////////
 // Get all categories
-router.get('/', categoryController.getAllCategories);
+router.get('/', authMiddleware, categoryController.getAllCategories);
 
+///////////////////// Admin /////////////////////
+// Create a new category
+router.post('/', authMiddleware, adminMiddleware, categoryController.createCategory);
 // Get a single category by ID
-router.get('/:categoryId', categoryController.getCategoryById);
-
+router.get('/:categoryId', authMiddleware, adminMiddleware, categoryController.getCategoryById);
 // Update a category by ID
-router.put('/:categoryId', categoryController.updateCategoryById);
-
+router.put('/:categoryId', authMiddleware, adminMiddleware, categoryController.updateCategoryById);
 // Delete a category by ID
-router.delete('/:categoryId', categoryController.deleteCategoryById);
+router.delete('/:categoryId', authMiddleware, adminMiddleware, categoryController.deleteCategoryById);
 
 module.exports = router;
