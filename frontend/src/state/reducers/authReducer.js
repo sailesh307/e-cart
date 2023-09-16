13bf2b50412f1cf1b7dbbe5300d08ab04d37587a
sailesh307 <sailesh953@gmail.com>
2023-09-16 09:20:28 +0530

    auth local storage error fixed

--- frontend/src/state/reducers/authReducer.js
+++ frontend/src/state/reducers/authReducer.js
@@ -3,7 +3,7 @@ import { LOGIN_SUCCESS, LOGOUT_SUCCESS } from '../actions/authActions';
 
 const initialState = {
     user: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null,
-    token: localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')) : null, // Add token property to store the authentication token
+    token: localStorage.getItem('token') ? localStorage.getItem('token') : null, // Add token property to store the authentication token
 };
 
 const authReducer = (state = initialState, action) => {
