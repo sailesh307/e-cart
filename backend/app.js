c2b7300152658cb5ba570ea6dedc20330090493c
sailesh307 <sailesh953@gmail.com>
2023-09-17 12:39:02 +0530

    code spliting

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const app = express();
+
+// Load environment variables from a .env file if needed
+dotenv.config();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB using mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Import route files
+const productRoutes = require('./routes/productRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
+
+// Use the routes
+app.get('/', (req, res) => {
+    res.send('This is e-cart api');
+});
+app.use('/api/products', productRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/transactions', transactionRoutes);
+
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+const app = require('./app'); // Import the Express app from app.js
 
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Load environment variables from a .env file if needed
-dotenv.config();
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-
-// Connect to MongoDB using mongoose
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-// Import route files
-const productRoutes = require('./routes/productRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const userRoutes = require('./routes/userRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-const transactionRoutes = require('./routes/transactionRoutes');
-
-// Use the routes
-app.get('/', (req, res) => {
-    res.send('This is e-cart api');
-});
-app.use('/api/products', productRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/transactions', transactionRoutes);
-// ...other server setup code...
 
 // Start the server
 app.listen(PORT, () => {
