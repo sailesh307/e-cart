71fc7cf8197a2d901131efaf2817816b81a104b1
sailesh307 <sailesh953@gmail.com>
2023-10-05 23:27:05 +0530

    logo and name changed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,14 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#282828', // dark
+          text: '#ffffff',    // text-white
+        },
+      },
+    },
   },
   plugins: [],
 });
